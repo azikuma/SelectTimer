4b1d5f9bfd844b1537ce617773cd934a3a11d9d4
azikuma <azikuma@gmail.com>
2021-06-05 17:12:06 +0900

    add viewModel

--- app/src/main/java/com/example/selecttimer/MainActivity.kt
+++ app/src/main/java/com/example/selecttimer/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.selecttimer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.example.selecttimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
 
         val setButton = findViewById<Button>(R.id.setButton)
         setButton.setOnClickListener {
@@ -20,12 +27,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun setTimerNum(){
 //        val hoursNum = findViewById<EditText>(R.id.hoursNum)
-        val minutesNum = findViewById<EditText>(R.id.minutesNum)
-        val secondsNum = findViewById<EditText>(R.id.secondsNum)
+//        val minutesNum = findViewById<EditText>(R.id.minutesNum)
+//        val secondsNum = findViewById<EditText>(R.id.secondsNum)
 
 //        var hours: String = hoursNum.text.toString()
-        var minutes: String = minutesNum.text.toString()
-        var seconds: String = secondsNum.text.toString()
+        var minutes: String = binding.minutesNum.text.toString()
+        var seconds: String = binding.secondsNum.text.toString()
 
 //        var millisTime = (hours.toInt() * 60 * 60 * 1000) + (minutes.toInt() * 60 * 1000) + (seconds.toInt() * 1000)
         var millisTime = (minutes.toInt() * 60 * 1000) + (seconds.toInt() * 1000)
--- app/src/main/java/com/example/selecttimer/SetTimerActivity.kt
+++ app/src/main/java/com/example/selecttimer/SetTimerActivity.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.TextView
+import com.example.selecttimer.databinding.ActivitySetTimerBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SetTimerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySetTimerBinding
+
     private lateinit var soundPool: SoundPool
     private var soundResId = 0
 
@@ -32,12 +35,15 @@ class SetTimerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_timer)
+        binding = ActivitySetTimerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_set_timer)
 
         val milliSeconds = intent.getStringExtra("milliseconds")?.toLong()
 
-        val timerText : TextView = findViewById(R.id.timerText)
-        val playStop = findViewById<FloatingActionButton>(R.id.playStop)
+//        val timerText : TextView = findViewById(R.id.timerText)
+//        val playStop = findViewById<FloatingActionButton>(R.id.playStop)
 
         if (milliSeconds != null) {
             setTimerText(milliSeconds)
@@ -45,19 +51,19 @@ class SetTimerActivity : AppCompatActivity() {
 //        val timer = milliSeconds?.toLong()?.let { MyCountDownTimer(it, 100) }
         val timer = milliSeconds?.let { MyCountDownTimer(it, 100) }
 
-        playStop.setOnClickListener {
+        binding.playStop.setOnClickListener {
             if (timer != null) {
                 timer.isRunning = when (timer.isRunning) {
                     true -> {
                         timer.cancel()
-                        playStop.setImageResource(
+                        binding.playStop.setImageResource(
                             R.drawable.ic_baseline_play_arrow_24
                         )
                         false
                     }
                     false -> {
                         timer.start()
-                        playStop.setImageResource(
+                        binding.playStop.setImageResource(
                             R.drawable.ic_baseline_stop_24
                         )
                         true
@@ -79,10 +85,10 @@ class SetTimerActivity : AppCompatActivity() {
         soundPool.release()
     }
 
-    private fun setTimerText(millisseconds: Long) {
-        val timerText : TextView = findViewById(R.id.timerText)
-        val minute = millisseconds / 1000L / 60L
-        val second = millisseconds / 1000L % 60L
-        timerText.text = "%1d:%2$02d".format(minute, second)
+    private fun setTimerText(milliseconds: Long) {
+//        val timerText : TextView = findViewById(R.id.timerText)
+        val minute = milliseconds / 1000L / 60L
+        val second = milliseconds / 1000L % 60L
+        binding.timerText.text = "%1d:%2$02d".format(minute, second)
     }
 }
